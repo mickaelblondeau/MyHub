6c3ee856e27b4423b4000db774b5974a1db88a5d
Mickael Blondeau <mickael.blondeau59@gmail.com>
2014-07-28 17:42:32 +0200

    Fix subscription show error & new tag not present

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -6,14 +6,15 @@ class SubscriptionController < ApplicationController
   end
 
   def show
-    authorize! :index, Channel
+    authorize! :show, Channel
     if params[:type] == 'u'
       user = User.friendly.find(params[:slug])
       @like = Like.where('owner_id = ? AND user_id = ?', current_user.id, user.id).first
     elsif params[:type] == 's'
       playlist = Playlist.friendly.find(params[:slug])
       @like = Like.where('owner_id = ? AND playlist_id = ?', current_user.id, playlist.id).first
-    else
+    end
+    if @like == nil
       redirect_to subscription_index_path
     end
   end
